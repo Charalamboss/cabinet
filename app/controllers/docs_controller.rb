3a1b3c13320f58d6ad760fee8c7e565aefdd0b45
Charalamboss <ch.pouroutides@gmail.com>
2019-01-06 12:46:03 +0200

    add edit destroy create methods

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -19,10 +19,18 @@ class DocsController < ApplicationController
 		end
 	end
 	def edit
+
 	end
 	def update
+		if @doc.update(doc_params)
+			redirect_to @doc
+		else
+			render 'edit'
+		end
 	end
 	def destroy
+		@doc.destroy
+		redirect_to docs_path
 	end
 
 	private
